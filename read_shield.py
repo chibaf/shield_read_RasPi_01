4b58f7c29984ce424f818d9e0a56d7504fa01008
chibaf <chibaf@mac.com>
2021-10-21 22:27:04 +0900

    Add files via upload

---
+++ read_shield.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import ADS1256
+import RPi.GPIO as GPIO
+
+ADC = ADS1256.ADS1256()
+ADC.ADS1256_init()
+adcv=[]
+ADC_Value = ADC.ADS1256_GetAll()
+for i in range(8):
+  adcv.append(ADC_Value[i]*5.0/0x7fffff)
+print(adcv)
+GPIO.cleanup()
+exit()
